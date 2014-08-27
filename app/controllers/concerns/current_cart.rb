0ab9b876f081ae477eb6a994355b3a8552cae466
daveferrara1 <daveferrara1@gmail.com>
2014-08-27 10:41:22 -0600

    cart added

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -6,16 +6,15 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Cart < ActiveRecord::Base
-  has_many :line_items, dependent: :destroy
+module CurrentCart
+  extend ActiveSupport::Concern
 
-  def add_product(product_id)
-    current_item = line_items.find_by(product_id: product_id)
-    if current_item
-      current_item.quantity += 1
-    else
-      current_item = line_items.build(product_id: product_id)
+  private
+
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
     end
-    current_item
-  end
 end
